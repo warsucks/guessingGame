c34c7821e69f98dc530cf37999e5100267133f1d
Kathy Lu <kaffy.lu@gmail.com>
2015-07-09 13:43:06 -0400

    adding guessManager.js

---
+++ js/guessManager.js
@@ -0,0 +1,40 @@
+$(document).ready(function()
+{
+	console.log("Document loaded");
+	var guess=[];
+	var submitButton = document.querySelector('#submitGuess');
+	submitButton.addEventListener('click',processInput);
+});
+
+function processInput(input)
+{
+	console.log("button clicked");
+	input.preventDefault();
+	var input = +$(this).closest('.form-inline').find('#guess').val();
+	console.log("Input: "+input);
+
+	if(validGuess(input))
+	{
+		printGuess(input);
+	}
+}
+
+function printGuess(guess)
+{
+	var guessNode = $('<li> guessed: '+guess+'</li>');
+	$('#pastGuesses').prepend(guessNode);
+	console.log("Tried to print guess: "+guess);
+}
+
+function validGuess(input)
+{
+	if(input>=1 && input<=100 && parseInt(input)===input)
+	{
+		return true;
+	}
+	else
+	{
+		console.log("Invalid guess. Guess must be a whole number");
+		return false;
+	}
+}
